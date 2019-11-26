ba45382addb88c228c77173e39ed49958bda09fd
Uriel Bitton <55159607+urielbitton@users.noreply.github.com>
2019-11-25 22:05:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-//Reactor JS
-//Used the concrete-abstract method to design ReactorJS
+/*Reactor JS
+*Version: 2.3.4 Stable
+*Description: Reactor JS is a highly modular and powerful framework designed to help * you create custom web designs with much less code.
+*Author: Uriel Bitton (Developed by Scorpion Edge). All rights reserved. MIT License 2019
+*/
+ 
 $(document).ready(function() {
-var appid = {};    
+
+//functions
+propsDesign();
+
+function propsDesign() {    
+//remove reactor models    
+$('head').append('<style>.reactor,reactor-vars, reactor-imgs{display:none!important}</style>'); 
+var appid = {};      
 var reactorapp = $('.reactorapp');
-var appnum = reactorapp.length;  
-for(i=0;i<appnum;i++) {    
-    appid[i] = $("[data-appid='"+i+"']");        
-}    
-    
+var appnum = reactorapp.length;       
+          
 //big appnum for loop     
 for(k=0;k<appnum;k++) {    
+$('.app').eq(k).addClass('app'+k);
 var featflex = {};      
 var featflexsize = $('.app'+k+' reactor-x').length;     
 var reactor = $('.app'+k+' .reactor').html();  
@@ -26,31 +35,34 @@ props = props.replace(new RegExp('{|}', 'g'),"");
 props = props.replace(/\s/g, '');  
 var propsarray = props.split(','); 
 var reactarray = {}; 
-var reactimgarray = {};   
-var replacer = {};      
-       
-for(i=1;i<=featflexsize;i++) {
-    featflex[i] = $('.app'+k+' reactor-x:nth-child('+i+')');
+var reactimgarray = {};    
+var replacer = {};       
+        
+for(i=0;i<featflexsize;i++) {
+    featflex[i] = $('.app'+k+' reactor-x:nth-child('+(i+1)+')');
     featflex[i].html(reactor);
     for(j=0;j<propsarray.length;j++) {
         reactarray[j] = featflex[i].data(propsarray[j]);
-    }
-    if($('reactor-imgs').length > 0) { 
+    } 
+    if($('.app'+k+' reactor-imgs').length > 0) { 
         for(j=0;j<imgpropsarray.length;j++) {
             reactimgarray[j] = featflex[i].data(imgpropsarray[j]);
-        }
-        for(j=0;j<imgpropsarray.length;j++) {
-            $('.app'+k+' reactor-x:nth-child('+i+') img:nth-child('+(j+1)+')').attr('src', '{'+imgpropsarray[j]+'}').attr('src', reactimgarray[j]);
-            //featflex[i].find('.reactorimg-'+imgpropsarray[j]).attr("src",reactimgarray[j]);
-        }  
-    }
+            $('.app'+k+' reactor-x:nth-child('+(i+1)+')').find('img:eq('+j+')').filter(function() {
+                return this.innerHTML = '{'+imgpropsarray[j]+'}';  
+            }).attr('src', reactimgarray[j]);   
+        }    
+    }     
     for(j=0;j<propsarray.length;j++) {
-        $('.app'+k+' reactor-x:nth-child('+i+') :contains('+propsarray[j]+')').html(reactarray[j]);  
-    }  
-} 
+        $('.app'+k+' reactor-x:nth-child('+(i+1)+')').find('*').filter(function() {
+            return this.innerHTML == '{'+propsarray[j]+'}';     
+        }).html(reactarray[j]); 
+    }    
+}  
 }
 //end of big appnum for loop  
-  
-$('head').append('<style>.reactor,reactor-vars, reactor-imgs{display:none!important}</style>'); 
+}//end of propsDesign function
+     
+    
+   
     
 }); 
