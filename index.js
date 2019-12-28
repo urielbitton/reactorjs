55f227b6debb12a6f0a2f6e28c232e9acf7253ca
Uriel Bitton <55159607+urielbitton@users.noreply.github.com>
2019-12-27 20:49:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,15 @@
  
 $(document).ready(function() {
 
-//functions
+//reactor functions
 propsDesign();
+liveWrite();
+beam();     
+debug();
 
 function propsDesign() {    
 //remove reactor models    
-$('head').append('<style>.reactor,reactor-vars, reactor-imgs{display:none!important}</style>'); 
+$('head').append('<style>.reactor,reactor-vars, reactor-imgs, reactor-container{display:none!important}</style>'); 
 var appid = {};      
 var reactorapp = $('.reactorapp');
 var appnum = reactorapp.length;       
@@ -19,8 +22,12 @@ var appnum = reactorapp.length;
 //big appnum for loop     
 for(k=0;k<appnum;k++) {    
 $('.app').eq(k).addClass('app'+k);
-var featflex = {};      
-var featflexsize = $('.app'+k+' reactor-x').length;     
+var featflex = {};    
+var rcont = $('reactor-container');    
+rcont = rcont.html(); 
+rcont = rcont.replace(/\s/g, '');
+rcont = rcont.replace(new RegExp('{|}', 'g'),"");     
+var featflexsize = $('.app'+k+' '+rcont).length;     
 var reactor = $('.app'+k+' .reactor').html();  
 var reactorvars = $('.app'+k+' reactor-vars');   
 if($('.app'+k+' reactor-imgs').length > 0) {      
@@ -29,7 +36,7 @@ var imgprops = reactorimgs.html();
 imgprops = imgprops.replace(/\s/g, '');
 imgprops = imgprops.replace(new RegExp('{|}', 'g'),"");  
 var imgpropsarray = imgprops.split(',');
-} 
+}  
 var props = reactorvars.html();    
 props = props.replace(new RegExp('{|}', 'g'),"");     
 props = props.replace(/\s/g, '');  
@@ -37,9 +44,9 @@ var propsarray = props.split(',');
 var reactarray = {}; 
 var reactimgarray = {};    
 var replacer = {};       
-        
+      
 for(i=0;i<featflexsize;i++) {
-    featflex[i] = $('.app'+k+' reactor-x:nth-child('+(i+1)+')');
+    featflex[i] = $('.app'+k+' '+rcont+':nth-of-type('+(i+1)+')');
     featflex[i].html(reactor);
     for(j=0;j<propsarray.length;j++) {
         reactarray[j] = featflex[i].data(propsarray[j]);
@@ -47,22 +54,70 @@ for(i=0;i<featflexsize;i++) {
     if($('.app'+k+' reactor-imgs').length > 0) { 
         for(j=0;j<imgpropsarray.length;j++) {
             reactimgarray[j] = featflex[i].data(imgpropsarray[j]);
-            $('.app'+k+' reactor-x:nth-child('+(i+1)+')').find('img:eq('+j+')').filter(function() {
+            $('.app'+k+' '+rcont+':nth-of-type('+(i+1)+')').find('img:eq('+j+')').filter(function() {
                 return this.innerHTML = '{'+imgpropsarray[j]+'}';  
             }).attr('src', reactimgarray[j]);   
         }    
     }     
     for(j=0;j<propsarray.length;j++) {
-        $('.app'+k+' reactor-x:nth-child('+(i+1)+')').find('*').filter(function() {
+        $('.app'+k+' '+rcont+':nth-of-type('+(i+1)+')').find('*').filter(function() {
             return this.innerHTML == '{'+propsarray[j]+'}';     
         }).html(reactarray[j]); 
     }    
 }  
-}
+}  
 //end of big appnum for loop  
 }//end of propsDesign function
      
     
+function liveWrite() { 
+var name; 
+var read = $('[re-read]');
+var write = $('[re-write]');    
+read.on('input', function() {
+    name = $(this).val();         
+    write.html(name);    
+});
+}
    
+    
+function beam() {
+    var beam = $('[re-beam]');
+    var page = [];
+    var i = 0;
+    beam.each(function() {
+        page[i] = $(this).attr('re-beam');
+        $(this).load(page[i]+' [re-app]'); 
+        ++i;
+    }); 
+      
+}
+   
+listRepeater();    
+function listRepeater() {
+    var lists = $('.reactor-list').length;
+    $('head').append('<style>list-repeat{display:none!important}</style>'); 
+    for(k=0;k<lists;k++) {
+        
+        var listrepeat = $('.reactor-list:nth-of-type('+(k+1)+') list-repeat');
+        listrepeat = listrepeat.html();
+        listrepeat = listrepeat.replace(new RegExp('{|}', 'g'),"");  
+        var listarray = listrepeat.split(',');
+        var listcontainer = $('.reactor-list:nth-of-type('+(k+1)+') [re-repeat]');
+        for(i=0;i<listarray.length;i++) {
+             listcontainer.append('<li>'+listarray[i]+'</li>');   
+        };
+    }
+}   
+   
+   
+//Debug tests
+
+function debug() {
+    if($('.reactortest').length) {
+        alert("reactor is live");
+    }   
+}    
+    
     
 }); 
